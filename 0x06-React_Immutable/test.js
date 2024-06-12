40529b3b9663c47756c749b9c67a3721db9d9859
Zouine2005 <zouinemohamade@gmail.com>
2024-06-12 14:28:31 +0100

    the test file

---
+++ 0x06-React_Immutable/test.js
@@ -0,0 +1,11 @@
+import getImmutableObject from './0-fromjs.js';
+
+const obj = {
+  fear: true,
+  smell: -1033575916.9145899,
+  wall: false,
+  thing: -914767132
+};
+
+const immutableObj = getImmutableObject(obj);
+console.log(immutableObj);
